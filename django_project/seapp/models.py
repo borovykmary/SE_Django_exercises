529f81e052d112ec5bfcc33135afa0d4820bb8d5
maryna <276069@student.pwr.edu.pl>
2024-11-09 14:12:49 +0100

    models and settings files modified

--- django_project/seapp/models.py
+++ django_project/seapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    products = models.ManyToManyField(Product, related_name='orders')
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.status}"
