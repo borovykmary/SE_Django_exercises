20973ebcdb0de490870416fa609b79e5daae674d
maryna <276069@student.pwr.edu.pl>
2024-11-13 10:08:58 +0100

    command file was fullfiled with samples

--- django_project/seapp/management/commands/populate_sample_data.py
+++ django_project/seapp/management/commands/populate_sample_data.py
@@ -11,17 +11,49 @@ class Command(BaseCommand):
         Order.objects.all().delete()
 
         product1 = Product.objects.create(
-            name='...',
-            price=19.99,
+            name='Sword of Tatsuka',
+            price=598.87,
             available=True
         )
+        product2 = Product.objects.create(
+            name='Samehada weapon',
+            price=785.99,
+            available=True
+        )
+        product3 = Product.objects.create(
+            name='kunai',
+            price=144.29,
+            available=False
+        )
         customer1 = Customer.objects.create(
-            name='...',
-            address='...'
+            name='Gaara',
+            address='Hidden Sand 21/4'
+        )
+        customer2  = Customer.objects.create(
+            name='Naruto',
+            address='Hidden Leaf 5/1o'
+        )
+        customer3 = Customer.objects.create(
+            name='Sasuke',
+            address='Hidden Leaf 18/22'
         )
         order1 = Order.objects.create(
             customer=customer1,
-            status='...'
+            status='new'
         )
         order1.products.add(product1)
+        self.stdout.write("Data created successfully.")
+
+        order2 = Order.objects.create(
+            customer=customer2,
+            status='in_process'
+        )
+        order2.products.add(product2)
+        self.stdout.write("Data created successfully.")
+
+        order3 = Order.objects.create(
+            customer=customer3,
+            status='sent'
+        )
+        order3.products.add(product3)
         self.stdout.write("Data created successfully.")
