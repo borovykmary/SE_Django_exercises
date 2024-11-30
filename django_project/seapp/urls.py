779b3ab196fdd0cd1c53111c1a302bd5bdaa22cb
maryna <276069@student.pwr.edu.pl>
2024-11-30 14:38:23 +0100

    Part 3 finished

--- django_project/seapp/urls.py
+++ django_project/seapp/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
-from .views import ProductViewSet, CustomerViewSet, OrderViewSet
 from rest_framework.routers import DefaultRouter
-
+from .views import ProductListView, ProductDetailView, ProductCreateView
+"""
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'customers', CustomerViewSet, basename='customer')
 router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [path('api/', include(router.urls))]
+"""
+
+urlpatterns = [
+    path('user/products/', ProductListView.as_view(),
+        name='product_list'),
+    path('user/products/<int:pk>/', ProductDetailView.as_view(),
+        name='product_detail'),
+    path('user/products/new/', ProductCreateView.as_view(),
+        name='product_create'),
+]
