4b13d570b7cc8e2eda581c251afb6b6d56e9b3be
maryna <276069@student.pwr.edu.pl>
2024-12-04 10:46:38 +0100

    Lab 7 finished

--- django_project/seapp/urls.py
+++ django_project/seapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from .views import ProductViewSet, CustomerViewSet, OrderViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'customers', CustomerViewSet, basename='customer')
 router.register(r'orders', OrderViewSet, basename='order')
 
-urlpatterns = [path('api/', include(router.urls))]
+urlpatterns = [path('api/', include(router.urls)),
+               path('api/token/', TokenObtainPairView.as_view(),
+                    name ='token_obtain_pair'),
+               path('api/token/refresh/', TokenRefreshView.as_view(),
+                    name ='token_refresh')
+               ]
